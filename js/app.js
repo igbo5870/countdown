10682226705a8394b20e0ff2523a8e735806b66f
Grégory <gregory.bouloc@gmail.com>
2018-01-03 11:46:49 +0100

    First commit - Mise en place de l'environnement

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*********************************************************
+ ** Nom du fichier : app.js                             **
+ ** Auteur : Grégory                                    **
+ ** Date de création : 03/01/2018                       **
+ ** Dernière mise à jour : 03/01/2018                   **
+ ** Objectif : Permettre de lancer un compte à rebours  **
+ *********************************************************/
