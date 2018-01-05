cfc29a3cb4a5bf026b7988390e94a473a99bd389
Grégory <gregory.bouloc@gmail.com>
2018-01-05 19:44:54 +0100

    ajout du formulaire pour l'interaction avec l'utilisateur

--- js/app.js
+++ js/app.js
@@ -12,6 +12,16 @@ function countDown(){
 	var html_heures = document.getElementById("id_heures");
 	var html_minutes = document.getElementById("id_minutes");
 	var html_secondes = document.getElementById("id_secondes");
+	var button_start = document.getElementById("button_start");
+
+	var input_heures = "0"//document.getElementById("input_heures").value;
+	var input_minutes = "0";
+	var input_secondes = "0";
+
+	var input_jours = document.getElementById("input_jours").value;
+	var select_mois = document.getElementById("id_mois").value;
+	var input_annee = document.getElementById("id_annee").value;
+	var horaire;
 
 	// Création d'une virable pour avoir la date actuelle 
 	//********************************************************
@@ -19,39 +29,47 @@ function countDown(){
 
 	// Création d'une variable pour avoir la date de l'évenement
 	//********************************************************
-	var date_evenement = new Date("Jan 1 00:00:00 2019");
+	var date_evenement = new Date("Jan" + " " +"1"+ " " + input_heures + ":" + input_minutes + ":" + input_secondes + " 2019");
 
-	/********************************************************
-	 **   calcule de la différence entre les deux dates    **
-	 ********************************************************/
-	var totale_secondes = (date_evenement - date_actuelle) / 1000;          // Diviser par 1000 permet de savoir																		 // les secondes totales restants 
 
-	// Nombre de jours
-	//*******************************************************
-	var jours = Math.floor(totale_secondes / ( 3600 * 24));
+	// Evenement au clicque 
+	//********************************************************
 
-	// Nombre d'heures
-	//*******************************************************
-	var heures = Math.floor(( totale_secondes - (jours * 3600 * 24 )) / 3600 );
 
-	// Nombre de minutes 
-	//*******************************************************
-	var minutes = Math.floor(( totale_secondes - ((jours * 3600 * 24 + heures * 3600))) / 60);
+	// 
+	// *******************************************************
+	function reload(){
 
-	// Nombre de secondes 
-	//*******************************************************
-	var secondes = Math.floor(( totale_secondes - ((jours * 3600 * 24 + heures * 3600 + minutes * 60))));
+		input_heures = document.getElementById("input_heures").value;
+		date_evenement = new Date( select_mois + " " + input_jours.toString() + " " +  input_heures + " " + input_annee.toString());
 
-	// Affichage du compte à rebours dans la page html 
-	// ******************************************************
-	//html_jours.innerHTML = jours + '';
-	html_heures.innerHTML = heures + '';
-	html_minutes.innerHTML =  minutes + '';
-	html_secondes.innerHTML =  secondes + '';
+		/********************************************************
+		 **   calcule de la différence entre les deux dates    **
+		 ********************************************************/
+		var totale_secondes = (date_evenement - date_actuelle) / 1000; 
+		var jours = Math.floor(totale_secondes / ( 3600 * 24));
+		var heures = Math.floor(( totale_secondes - (jours * 3600 * 24 )) / 3600 );
+		var minutes = Math.floor(( totale_secondes - ((jours * 3600 * 24 + heures * 3600))) / 60);
+		var secondes = Math.floor(( totale_secondes - ((jours * 3600 * 24 + heures * 3600 + minutes * 60))));
 
+		// Affichage du compte à rebours dans la page html 
+		// ******************************************************
+		html_jours.innerHTML = jours + '';
+		html_heures.innerHTML = heures + '';
+		html_minutes.innerHTML =  minutes + '';
+		html_secondes.innerHTML =  secondes + '';
+
+
+	}
+
+	button_start.addEventListener('click', function() {
+		reload();
+		
+	});
+	
 	// Rafraichissement de l'image toutes les secondes 
 	//*******************************************************
-	 var actualisation = setTimeout("countDown();", 1000);
+	setInterval(countDown , 1000);
 }
 
 countDown();
